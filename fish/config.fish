b66d3644b0bb6a44b41b7f1ba351573d85f94da6
kumar vaibhav <kv26102009@gmail.com>
2025-11-05 15:37:29 +0530

    sway dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+zoxide init fish | source
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    alias pamcan pacman
+    alias ls 'eza --icons'
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+    alias v nvim
+    alias x clear
+    alias lt 'eza --tree --icons'
+    alias cd z
+    alias rm 'rm -rf'
+end
+
+fastfetch
